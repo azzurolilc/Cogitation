76c1ae813a8cf43bf1216467feabbc5af0b5bd4b
torrac <torrac1115@gmail.com>
2016-10-22 12:34:50 -0500

    update ScoreSystem and User class

--- Assets/Scripts/Subjects/common/ScoreSystem.cs
+++ Assets/Scripts/Subjects/common/ScoreSystem.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class ScoreSystem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	int level, score;
+
+	public ScoreSystem() {
+		this.level = 0;
+		this.score = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
