c361610b4ee1413a4ef9f94534c89460939bcc3e
torrac <torrac1115@gmail.com>
2016-10-22 17:41:13 -0500

    try build

---
+++ Assets/Scripts/Subjects/common/AnswerButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Subjects/common/ScoreSystem.cs
+++ Assets/Scripts/Subjects/common/ScoreSystem.cs
@@ -10,4 +10,16 @@ public class ScoreSystem : MonoBehaviour {
 		this.score = 0;
 	}
 
+	public void firstRight(){
+		this.score += 2;
+		updateLevel ();
+	}
+
+	public void secondRight(){
+		this.score += 1;
+		updateLevel ();
+	}
+	private void updateLevel(){
+		this.level = this.score / 10;
+	}
 }
