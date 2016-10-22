2d14a33e748291e9a2e90dd0c165443f5308a3b7
torrac <torrac1115@gmail.com>
2016-10-22 18:31:25 -0500

    update MainScene

--- Assets/Scripts/Subjects/common/AnswerButton.cs
+++ Assets/Scripts/Subjects/common/AnswerButton.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AnswerButton : MonoBehaviour {
+//public class AnswerButton : MonoBehaviour, IGvrGazeResponder{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+//	bool isAnswer, clicked;
+
+
+
+//}
